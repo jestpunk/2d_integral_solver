b4489b2c2e992c0ef8ef87454b62cfa10cf98b70
Nikita Vasiliev <nikita.vasiliev@math.msu.ru>
2024-06-01 01:44:25 +0300

    Add argv interface

--- main.c
+++ main.c
@@ -6,25 +6,43 @@
 #include "fancy_output.h"
 #include "utils.h"
 
-/* 3 */
+// USAGE
+// ./2D_integral_solver setup N x_min x_max y_min y_max
+//
 int main(int argc, const char** argv) {
+    int N;
+    double x_min;
+    double x_max;
+    double y_min;
+    double y_max;
+    
+    FILE* fp;
+
+    struct point* points;
+    struct triangle* triangles;
+    struct edge* edges;
+
+    char buf[1000];
+    double true_integral_value = 23. / 45.;
+    double calculated_integral_value;
+
     if (argc < 2) {
         printf("Too few args\n");
         return -1;
     }
 
     if (strcmp(argv[1], "setup") == 0) {
-        int N = 100;
-        double x_min = 0.;
-        double x_max = 1.;
-        double y_min = 0.;
-        double y_max = 1.;
-        FILE* fp;
+        if (argc < 7) {
+            printf("Too few args\n");
+            return -1;
+        }
 
-        struct point* points;
-        struct triangle* triangles;
-        struct edge* edges;
-
+        N = atoi(argv[2]);
+        x_min = atof(argv[3]);
+        x_max = atof(argv[4]);
+        y_min = atof(argv[5]);
+        y_max = atof(argv[6]);
+        
         points = (struct point*)malloc((N + 1) * (N + 1) * sizeof(struct point));
         triangles = (struct triangle*)malloc(2 * N * N * sizeof(struct triangle));
         edges = (struct edge*)malloc(((4 * N) + 2 * N * (N - 1) + N * N) * sizeof(struct edge));
@@ -59,18 +77,6 @@ int main(int argc, const char** argv) {
         return 0;
     }
     else if (strcmp(argv[1], "calculate") == 0) {
-        int N;
-        
-        FILE* fp;
-
-        struct point* points;
-        struct triangle* triangles;
-        struct edge* edges;
-
-        char buf[1000];
-        double true_integral_value = 23. / 45.;
-        double calculated_integral_value;
-
         { // initialization
         fp = fopen("output.txt", "r");
         if(!fp) {
@@ -90,9 +96,9 @@ int main(int argc, const char** argv) {
         }
 
         scan_from_file(fp, points, triangles, edges, &N);
-        print_points(points, N);
-        print_triangles(triangles, N);
-        print_edges(edges, N);
+        //print_points(points, N);
+        //print_triangles(triangles, N);
+        //print_edges(edges, N);
         }
 
         calculated_integral_value = calc_integral(triangles, points, N);
